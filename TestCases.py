0a913c5653fd5105bc73d0d1f2251d89dad55f70
Christopher Yi <yi.christopher@hotmail.com>
2022-04-29 12:24:34 -0400

    Added error messages from Mohammed's test file. Scope of tests is still the same, we must add more comprehensive ones.

--- TestCases.py
+++ TestCases.py
@@ -10,11 +10,12 @@ class TestRunner(unittest.TestCase):
         Tests initializing driver with predefined values and logging in to Bright.
         Reliant on stable internet and valid predefined credentials.
         """
-        MLS_test = MLSBot("csosasan", "Sos@S@nchez31543", "Chrome", "Bright", "", 100, 22003, 1)
-        MLS_test.initDriver()
-        MLS_test.loginMLS()
-        title = MLS_test.driver.title
-        self.assertEqual(title, 'Dashboard | Bright MLS')
+        message = "Bright Login Failed, Dashboard not Detected"
+        self.MLS_test = MLSBot("csosasan", "Sos@S@nchez31543", "Chrome", "Bright", "", 100, 22003, 1)
+        self.MLS_test.initDriver()
+        self.MLS_test.loginMLS()
+        title = self.MLS_test.driver.title
+        self.assertEqual(title, 'Dashboard | Bright MLS', message)
 
     def test_facebookNav(self):
         """
@@ -22,11 +23,12 @@ class TestRunner(unittest.TestCase):
         Test initializing driver with predefined values and logging in to Facebook.
         Reliant on stable internet and valid predefined credentials.
         """
-        FB_test = MarketBot("mobhuiyan1998@yahoo.com", "Orpon1998!", "Chrome")
-        FB_test.initDriver()
-        FB_test.loginFB()
-        title = FB_test.driver.title
-        self.assertEqual(title, 'Facebook')
+        message = "Facebook Login Failed, Dashboard not Detected"
+        self.FB_test = MarketBot("mobhuiyan1998@yahoo.com", "Orpon1998!", "Chrome")
+        self.FB_test.initDriver()
+        self.FB_test.loginFB()
+        title = self.FB_test.driver.title
+        self.assertEqual(title, 'Facebook', message)
 
 if __name__ == '__main__':
     unittest.main()
