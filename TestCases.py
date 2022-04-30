8ad39f6ec3ba30702090025131c6900adac923c6
Mohammed Bhuiyan <mobhuiyan1998@gmail.com>
2022-04-30 14:03:17 -0400

    Added driver.close() on the two tests added

--- TestCases.py
+++ TestCases.py
@@ -1,5 +1,7 @@
+from xml.dom.expatbuilder import parseString
 from Main_Runner import MLSBot
 from Main_Runner import MarketBot
+import Main_Runner
 import unittest
 import os
 import time
@@ -55,12 +57,14 @@ class TestRunner(unittest.TestCase):
         self.FB_test.driver.close()
 
     def test_getDefaultCSV(self):
-
+        
         message = "Default CSV not detected."
+
         self.MLS_test = MLSBot("csosasan", "Sos@S@nchez31543", "Chrome", "Bright", "", 100, 22003, 1)
         self.MLS_test.initDriver()
         self.MLS_test.loginMLS()
         self.MLS_test.getDefaultCSV()
+
         csv_wait = 0
         while not os.path.exists(self.MLS_test.data_path + 'Agent One-Line.csv'):
             time.sleep(1)
@@ -70,6 +74,87 @@ class TestRunner(unittest.TestCase):
         csv_exists = os.path.exists(self.MLS_test.data_path + 'Agent One-Line.csv')
         self.assertTrue(csv_exists, message)
         self.MLS_test.driver.close()
+
+    def test_imgDownloader(self):
+        """
+        Tests that the imgDownloader method properly creates the img directory and
+        downloads the pictures with the given MLS NUM, 
+        """
+
+        self.MLS_test = MLSBot("csosasan", "Sos@S@nchez31543", "Chrome", "Bright", "", 100, 22003, 1)
+        self.MLS_test.initDriver()
+        self.MLS_test.loginMLS()
+
+        MLS_NUM = "VAFX2048786"
+
+        self.MLS_test.pullListingImg(MLS_NUM)
+        self.MLS_test.driver.close() 
+
+        for i in range(0,18):
+            if(os.path.exists(MLS_NUM + "-" + str(i))):
+                continue
+            else: 
+                message = "Error at number" + str(i)
+                self.assertFalse(False,message)
+
+        self.assertTrue(True)
+            
+    #def test_pullListings(self):
+    #    """
+    #    Tests that the search queries are properly pulled 
+    #    and inserted in the CSV
+    #    """
+    # 
+    #
+    #    self.MLS_test = MLSBot("csosasan", "Sos@S@nchez31543", "Chrome", "Bright", "", 100, 22003, 1)
+    #    self.MLS_test.initDriver()
+    #    self.MLS_test.loginMLS()
+    #    self.MLS_test.pullListings()
+
+
+    def test_addListingsFaceBook(self): 
+        """
+
+        Test automated listings on Facebook with predefined images and listing data 
+        are correctly posted on the Facebook Marketplace
+        """
+
+        message = "Facebook Listing Not Detected"
+        self.FB_test = MarketBot("mobhuiyan1998@yahoo.com", "Orpon1998!", "Chrome")
+        self.FB_test.initDriver()
+        self.FB_test.loginFB()   
+
+        self.MLS_test = MLSBot("csosasan", "Sos@S@nchez31543", "Chrome", "Bright", "", 100, 22003, 1)
+        self.MLS_test.initDriver()
+        self.MLS_test.loginMLS()
+
+
+        num_baths = 1
+        num_beds = 1
+        price = "210,000"
+        address = "2059 HUNTINGTON AVENUE Alexandria, VA"
+        description = "VAFX2048786"
+        MLS_NUM = description
+
+
+        self.FB_test.createListingFromMLS(
+            self.MLS_test,
+            MLS_NUM,"Unit/Flat/Apartment",
+            num_baths,num_beds,
+        price,address,description)
+
+        try: 
+            self.assertTrue(self.FB_test.checkIfListed(MLS_NUM))
+        except:
+            pass
+        finally: 
+            Main_Runner.remove_list.append(MLS_NUM)
+            self.FB_test.deleteListings()
+            self.FB_test.driver.close()
+            self.MLS_test.driver.close()
+        
+
+    
 
 if __name__ == '__main__':
     unittest.main()
